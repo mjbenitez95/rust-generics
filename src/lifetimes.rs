83578e4083d97bea70062e1b7a8251243249c1f4
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-02-01 12:35:02 -0800

    add example of unacceptable lifetime

--- src/lifetimes.rs
+++ src/lifetimes.rs
@@ -43,3 +43,14 @@ fn acceptable_overlapping_lifetimes() {
         println!("The longest string is {}", result);
     }
 }
+
+fn unacceptable_lifetime() {
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+    }
+    // this line would fail because result only lives as long as string2
+    // println!("The longest string is {}", result);
+}
