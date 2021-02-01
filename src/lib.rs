117875693654840b53c7c55a2de1ff8f1b7f03f7
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-02-01 11:04:52 -0800

    add example of defining default behavior for a trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
 }
 
 pub struct NewsArticle {
@@ -7,12 +9,6 @@ pub struct NewsArticle {
     pub location: String,
     pub author: String,
     pub content: String,
-}
-
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
-    }
 }
 
 pub struct Tweet {
@@ -37,4 +33,16 @@ fn main() {
     };
 
     println!("1 new tweet! {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
 }
