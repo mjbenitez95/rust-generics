e3c2f2ac288bcc1f5e472837a2846aa25f159ea4
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-02-01 09:20:47 -0800

    remove duplication by extracting function

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,22 @@ fn main() {
 
 fn extract_function() {
     let number_list = vec![34, 50, 25, 100, 65];
-
-    let mut largest = number_list[0];
-
-    for number in number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
-
-    println!("The largest number is {}!", largest);
+    let result = largest(&number_list);
+    println!("The largest number is {}!", result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let result = largest(&number_list);
+    println!("The largest number is {}!", result);
+}
 
-    let mut largest = number_list[0];
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
 
-    for number in number_list {
-        if number > largest {
-            largest = number;
+    for item in list {
+        if item > largest {
+            largest = item;
         }
     }
 
-    println!("The largest number is {}!", largest);
+    largest
 }
