759f2edcdf5390eeb4e7e4a547c2e999ff3f0c31
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-02-01 09:34:10 -0800

    add example of generic struct and comment noncompiling code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     extract_function();
-    println!();
     generic_data_types();
+    generic_struct();
 }
 
 fn extract_function() {
@@ -30,21 +30,32 @@ fn generic_data_types() {
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
     let char_list = vec!['y', 'm', 'a', 'c', 'q', 'e'];
 
-    let largest_number = largest_generic(&number_list);
-    let largest_char = largest_generic(&char_list);
+    // let largest_number = largest_generic(&number_list);
+    // let largest_char = largest_generic(&char_list);
 
-    println!("The largest number is {}!", largest_number);
-    println!("The largest char is {}!", largest_char);
+    // println!("The largest number is {}!", largest_number);
+    // println!("The largest char is {}!", largest_char);
 }
 
-fn largest_generic<T>(list: &[T]) -> &T {
-    let mut largest = &list[0];
+// fn largest_generic<T>(list: &[T]) -> &T {
+//     let mut largest = &list[0];
 
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
 
-    largest
+//     largest
+// }
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+fn generic_struct() {
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+    // let wont_work = Point { x: 5, y: 4.0 };
 }
