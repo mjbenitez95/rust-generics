25416d0f314800a056dcd49ebb2f644a1d7fae9f
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-02-01 12:33:34 -0800

    add examples of different lifetimes being passed in

--- src/lifetimes.rs
+++ src/lifetimes.rs
@@ -22,9 +22,24 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 }
 
 pub fn main() {
+    basic_lifetimes();
+    acceptable_overlapping_lifetimes();
+}
+
+fn basic_lifetimes() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
+}
+
+fn acceptable_overlapping_lifetimes() {
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
 }
