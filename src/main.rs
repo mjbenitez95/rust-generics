c7c8a16649658362c35b5c6bfc4c76bd8bb190c0
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-02-01 09:31:10 -0800

    add example of generic function that will not compile

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
     extract_function();
+    println!();
+    generic_data_types();
 }
 
 fn extract_function() {
@@ -13,6 +15,29 @@ fn extract_function() {
 }
 
 fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn generic_data_types() {
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let char_list = vec!['y', 'm', 'a', 'c', 'q', 'e'];
+
+    let largest_number = largest_generic(&number_list);
+    let largest_char = largest_generic(&char_list);
+
+    println!("The largest number is {}!", largest_number);
+    println!("The largest char is {}!", largest_char);
+}
+
+fn largest_generic<T>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
     for item in list {
